fef3c47cc3389e47e6a23ce60d5a3a1059df370f
Martin Ro <martin-ro@users.noreply.github.com>
2024-09-12 13:53:42 +0800

    pint

--- src/Commands/MakeBlockCommand.php
+++ src/Commands/MakeBlockCommand.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 namespace MartinRo\FilamentBlocks\Commands;
 
 use Filament\Support\Commands\Concerns\CanIndentStrings;
--- src/Facades/FilamentBlocks.php
+++ src/Facades/FilamentBlocks.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 namespace MartinRo\FilamentBlocks\Facades;
 
 use Illuminate\Support\Facades\Facade;
